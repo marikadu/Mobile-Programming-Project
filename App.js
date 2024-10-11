dab908271ed17c90cce14758c884600915ec2278
marikadu <marika.duhhanina@student.hamk.fi>
2024-10-11 09:54:52 +0300

    tab for testing

--- App.js
+++ App.js
@@ -179,6 +179,7 @@ export default function App({ navigation }) {
         {/* calling the TimerComplete function for the badge */}
         {(props) => <TimerScreen {...props} onTimerEnd={handleTimerEnd} />}
       </Tab.Screen>
+      <Tab.Screen name="Feedback" component={FeedbackScreen} />
     </Tab.Navigator>
   </NavigationContainer>
     
