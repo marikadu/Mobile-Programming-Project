8fe06aeeb5d44b6689fc0c2a3be274a83968c3a7
marikadu <marika.duhhanina@student.hamk.fi>
2024-10-24 19:13:12 +0300

    replaced Alert with custom Modal
    because Alert cannot be customised, so similar logic as delete confirmation

--- components/AddressScreen.js
+++ components/AddressScreen.js
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from 'react';
 const targetURL = "https://pepperonipals.lm.r.appspot.com";
 // const targetURL = 'http://10.0.2.2:8080'
 
-export default function AddressScreen( route, navigation ) {
+export default function AddressScreen(route, navigation) {
   const [text, setText] = useState('');
   const [newAddressLine1, setAddressLine1] = useState('');
   const [newAddressLine2, setAddressLine2] = useState('');
@@ -15,28 +15,38 @@ export default function AddressScreen( route, navigation ) {
   const [selectedAddress, setSelectedAddress] = useState(null);  // Store selected address for deletion
   const [isDeleteModalVisible, setDeleteModalVisible] = useState(false);  // Control the modal visibility
 
+  // creating a custom Modal that mimics Alert window's behaviour
+  // because the Alert cannot be directly customisible, according to our research
+  const [isAlertVisible, setAlertVisible] = useState(false);
+  const [alertTitle, setAlertTitle] = useState('');
+  const [alertMessage, setAlertMessage] = useState('');
+
+
   // Fetch addresses when the component mounts
   useEffect(() => {
     readAllAddresses();
     clearInputFields();  // Clear form after saving
-  }, []);  
+  }, []);
 
   // Function to save the address
   const saveAddress = () => {
     const newAddress = {
-        addressLine1: newAddressLine1,
-        addressLine2: newAddressLine2,
-        city: newCity,
-        postcode: newPostcode,
+      addressLine1: newAddressLine1,
+      addressLine2: newAddressLine2,
+      city: newCity,
+      postcode: newPostcode,
     };
 
-      addAddress(newAddress).then(() => {
-        Alert.alert("Address Saved", "Your address has been added!"); // "Alert.alert()"" instead of "alert" to also change the title of the alert window
-        readAllAddresses();  // Refresh the list after adding a new address
-        clearInputFields();  // Clear form after saving
+    addAddress(newAddress).then(() => {
+      // Alert.alert("Address Saved", "Your address has been added!"); // "Alert.alert()"" instead of "alert" to also change the title of the alert window
+      // custom alert window, above is the old and original one ^
+      showAlert("Address Saved", "Your address has been added!");
+
+      readAllAddresses();  // Refresh the list after adding a new address
+      clearInputFields();  // Clear form after saving
     }).catch((error) => {
-        console.error('Error saving order:', error);
-        alert('Failed to save the address. Please try again.');
+      console.error('Error saving order:', error);
+      showAlert("Error", "Failed to save the address. Please try again!");
     });
   };
 
@@ -60,7 +70,6 @@ export default function AddressScreen( route, navigation ) {
   // UPDATE Address in mongoDB FUNCTION
   const updateAddressInDb = async () => {
     console.log('Update id: ' + updateId);
-    // console.log('Update fish: ' + newFish.breed);
     if (!updateId) return; // If no item is selected to update, return
     setAddressList(addressList => addressList.filter(address => address._id != updateId));
     try {
@@ -95,34 +104,35 @@ export default function AddressScreen( route, navigation ) {
   const confirmDeleteAddress = () => {
     if (selectedAddress) {
       // deleteAddress(selectedAddress.id).then(() => {
-        deleteOneAddress(selectedAddress._id).then(() => {
-        alert("Address deleted successfully.");
+      deleteOneAddress(selectedAddress._id).then(() => {
+        // alert("Address deleted successfully."); // OLD alert
+        showAlert("Address deleted", "Address deleted successfully."); // new alert
         readAllAddresses();  // Refresh the list after deleting
         setDeleteModalVisible(false);  // Hide the modal
         setSelectedAddress(null);  // Clear the selected address
       }).catch((error) => {
         console.error('Error deleting address:', error);
-        alert('Failed to delete the address. Please try again.');
+        showAlert("Error", "Failed to delete the address. Please try again."); // new alert
       });
     }
   };
 
-    // Delete Address from MongoDB
-    const deleteOneAddress = async id => {
-      console.log('Delete id: ' + id);
-      try {
-        let response = await fetch(
-          targetURL + '/deleteoneaddress/' + id,
-          {
-            method: 'DELETE',
-          },
-        );
-        let json = await response.json();
-        setAddressList(json);
-      } catch (error) {
-        console.log(error);
-      }
-    };
+  // Delete Address from MongoDB
+  const deleteOneAddress = async id => {
+    console.log('Delete id: ' + id);
+    try {
+      let response = await fetch(
+        targetURL + '/deleteoneaddress/' + id,
+        {
+          method: 'DELETE',
+        },
+      );
+      let json = await response.json();
+      setAddressList(json);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
 
 
@@ -134,7 +144,7 @@ export default function AddressScreen( route, navigation ) {
 
   // READING Address Data from MongoDB
   const readAllAddresses = async () => {
-    
+
     try {
       let response = await fetch(
         // 'http://10.0.2.2:8080/readmenu', // FROM MONGODB OR GOOGLE CLOUD
@@ -173,31 +183,38 @@ export default function AddressScreen( route, navigation ) {
     setUpdateId(null);  // Exit update mode
   };
 
+  // custom alert proterties
+  const showAlert = (title, message) => {
+    setAlertTitle(title);
+    setAlertMessage(message);
+    setAlertVisible(true);
+  };
+
   // Display the Address details
   const renderAddress = () => {
     return (
       <View>
         <Text style={styles.text}> Address Details:</Text>
-          <TextInput style={styles.addressItemStyle}
+        <TextInput style={styles.addressItemStyle}
           placeholder="Address line 1"
           value={newAddressLine1}
           onChangeText={setAddressLine1}
-          ></TextInput>
-          <TextInput style={styles.addressItemStyle}
+        ></TextInput>
+        <TextInput style={styles.addressItemStyle}
           placeholder="Address line 2"
           value={newAddressLine2}
           onChangeText={setAddressLine2}
-          ></TextInput>
-          <TextInput style={styles.addressItemStyle}
+        ></TextInput>
+        <TextInput style={styles.addressItemStyle}
           placeholder="City"
           value={newCity}
           onChangeText={setCity}
-          ></TextInput>
-          <TextInput style={styles.addressItemStyle}
+        ></TextInput>
+        <TextInput style={styles.addressItemStyle}
           placeholder="Postcode"
           value={newPostcode}
           onChangeText={setPostcode}
-          ></TextInput>
+        ></TextInput>
       </View>
     );
   };
@@ -220,8 +237,8 @@ export default function AddressScreen( route, navigation ) {
       </View>
 
       <KeyboardAvoidingView // KeyboardAvoidingView makes the elements stay on their place when keyboard opens
-      style={styles.screenContainer}
-      behavior="padding"
+        style={styles.screenContainer}
+        behavior="padding"
       >
         <Text style={styles.text}>Saved Addresses:</Text>
         {/* Address List */}
@@ -273,28 +290,37 @@ export default function AddressScreen( route, navigation ) {
           </View>
         </View>
       </Modal>
+
+      {/* custom alert window */}
+      <Modal
+        visible={isAlertVisible}
+        transparent={true}
+        animationType="fade"
+        onRequestClose={() => setAlertVisible(false)}
+      >
+        <View style={styles.customAlertBackground}>
+          <View style={styles.customAlertContainer}>
+            <Text style={styles.customAlertTitle}>{alertTitle}</Text>
+            <Text style={styles.customAlertMessage}>{alertMessage}</Text>
+            <TouchableHighlight
+              style={styles.customAlertButton}
+              onPress={() => setAlertVisible(false)}
+              underlayColor="#cc7333"
+            >
+              <Text style={styles.customAlertButtonText}>OK</Text>
+            </TouchableHighlight>
+          </View>
+        </View>
+      </Modal>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  background:{
-    backgroundColor: "#fff",
-  },
-  screenContainer:{
+  screenContainer: {
     // flex:1,
-    backgroundColor:"#fff", // covers the grey background
-  },
-  listItemStyle: {
-    flex: 1,
-    borderWidth: 2,
-    borderColor: '#CD6524',
-    padding: 5,
-    backgroundColor: '#FFF9F2',
-    borderRadius: 20,
-    margin: 10,
-    alignSelf: 'center',
-    alignItems: 'center',
+    backgroundColor: "#fff", // covers the grey background
   },
   text: {
     color: '#CD6524',
@@ -316,17 +342,6 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     width: 220,
   },
-  addressItemStylePayment: {
-    borderWidth: 2,
-    borderColor: '#FFF9F2',
-    padding: 10,
-    backgroundColor: '#FFE8D8',
-    borderRadius: 10,
-    margin: 10,
-    paddingLeft: 40,
-    paddingRight: 40,
-    alignSelf: 'center',
-  },
   listStyle: {
     alignItems: 'center',
     width: '100%',
@@ -342,34 +357,6 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     alignSelf: 'center',
   },
-  itemText: {
-    color: '#CD6524',
-    fontSize: 20,
-    margin: 2,
-  },
-  flatliststyle: {
-    width: '100%', // makes it the whole page and grey-ish lines from the sides disappear!
-    backgroundColor: '#FFFFFF',
-  },
-  itemContainer: {
-    padding: 5,
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  textContainer: {
-    marginLeft: 20,
-    flex: 1,
-  },
-  descriptionText: {
-    flex: 1,
-    color: '#CD6524',
-    fontSize: 16,
-  },
-  pizzaImage: {
-    width: 80,
-    height: 80,
-    borderRadius: 25,
-  },
   button: {
     margin: 20,
     paddingTop: 10,
@@ -380,7 +367,7 @@ const styles = StyleSheet.create({
     alignSelf: "center",
   },
   buttonText: {
-    flex:1,
+    flex: 1,
     fontSize: 24,
     fontWeight: 'bold',
     color: '#fff',
@@ -407,7 +394,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
 
   }, // modal styling
-  modalBackground: { 
+  modalBackground: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
@@ -418,15 +405,17 @@ const styles = StyleSheet.create({
     padding: 24,
     backgroundColor: '#fff',
     borderRadius: 10,
-    elevation: 10, // shadow under the container
+    elevation: 10, // shadow behind the window
   },
   modalTitle: {
+    color: '#CD6524',
     fontSize: 20,
     fontWeight: 'bold',
     bottom: 8,
     marginBottom: 10,
   },
   modalMessage: {
+    color: '#CD6524',
     fontSize: 18,
     marginBottom: 20,
   },
@@ -448,5 +437,43 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
     textAlign: 'center',
+  },
+  // custom alert window styling
+  customAlertBackground: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // half transparent background
+  },
+  customAlertContainer: {
+    width: 300,
+    padding: 20,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    alignItems: 'center',
+    elevation: 10, // shadow behind the window
+  },
+  customAlertTitle: {
+    color: '#CD6524',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  customAlertMessage: {
+    color: '#CD6524',
+    fontSize: 18,
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  customAlertButton: {
+    backgroundColor: '#F58C41',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  customAlertButtonText: {
+    fontSize: 18,
+    color: '#fff',
+    fontWeight: 'bold',
   },
 });
